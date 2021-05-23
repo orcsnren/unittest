eaff96f72437c78dafbf8b058a2bbe35c5a6d34c
Soner <sonereren39@gmail.com>
2021-05-23 19:35:58 +0300

    student test with assertj assertions

---
+++ src/test/java/org/soner/course/unittest/courserecord/StudentNestedTestWithAssertJAssertions.java
@@ -0,0 +1,138 @@
+package org.soner.course.unittest.courserecord;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("student")
+@DisplayName("Student Test With Nested Tests")
+public class StudentNestedTestWithAssertJAssertions {
+
+//    @RegisterExtension
+//    static TestLoggerExtension testLoggerExtension = new TestLoggerExtension();
+
+    @Nested
+    @DisplayName("Create Student")
+    @Tag("createStudent")
+    class CreateStudent {
+
+        @Test
+        @DisplayName("Test every student must have an id, name and surname")
+        void shouldCreateStudentWithIdNameAndSurname() {
+
+            final Student soner = new Student("2", "Soner", "Eren");
+            Student orcun = new Student("1", "Orcun", "Eren");
+            Student student = orcun;
+
+            assertThat(orcun).as("Orcun")
+                    .isSameAs(student)
+                    .isNotSameAs(soner)
+                    .extracting(Student::getName)
+                    .asString()
+                    .isEqualTo("Orcun")
+                    .isNotEqualTo("orcunt")
+                    .startsWith("O")
+            ;
+
+            assertThat(new Student("id1", "Orcun", "Erener").getName()).as("Orcun")
+                    .doesNotEndWith("M")
+            ;
+
+            assertThat(List.of(orcun, soner))
+                    .extracting(Student::getName)
+                    .containsOnly("Orcun", "Soner")
+            ;
+        }
+
+        @Test
+        @DisplayName("Test every student must have an id, name and surname with grouped assertions")
+        void shouldCreateStudentWithIdNameAndSurnameWithGroupedAssertions() {
+
+            // In a grouped assertion all assertions are executed,
+            Student orcun = new Student("1", "Orcun", "Eren");
+
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat(orcun.getName()).as("orcun's name").isEqualTo("Orcun");
+            softAssertions.assertThat(orcun.getName()).as("orcunt's name").isNotEqualTo("orcunt");
+            softAssertions.assertAll();
+
+            // and any failures will be reported together.
+            assertAll("Student's name character check",
+                    () -> assertTrue(orcun.getName().startsWith("O")),
+                    () -> assertTrue(orcun.getName().startsWith("O"), () -> "Student's name " + "starts with Or"),
+                    () -> assertFalse(() -> {
+                        Student naz = new Student("id1", "Naz", "Ayhan");
+                        return naz.getName().endsWith("M");
+                    }, () -> "Student's name " + "ends with M")
+            );
+
+            assertAll(() -> {
+                        final Student soner = new Student("2", "Soner", "Eren");
+
+                        assertArrayEquals(new String[]{"Orcun", "Soner"}, Stream.of(orcun, soner).map(Student::getName).toArray());
+                    },
+                    () -> {
+                        Student student = orcun;
+                        final Student soner = new Student("2", "Soner", "Eren");
+
+                        assertSame(orcun, student); // orcun == student
+                        assertNotSame(student, soner);
+                    });
+        }
+    }
+
+    @Nested
+    @DisplayName("Add Course to Student")
+    @Tag("addCourse")
+    class AddCourseToStudent {
+        @Test
+        @DisplayName("Add course to a student less than 10ms")
+        void addCourseToStudentWithATimeConstraint() {
+
+            assertTimeout(Duration.ofMillis(10), () -> {
+                //nothing will be done and this code run under 10ms
+            });
+
+            final String result = assertTimeout(Duration.ofMillis(10), () -> {
+                return "some string result";
+            });
+            assertEquals("some string result", result);
+
+            final Student student = new Student("1", "Soner", "Eren");
+            LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(null, null);
+            assertTimeout(Duration.ofMillis(10), () -> student.addCourse(lecturerCourseRecord));
+
+            assertTimeoutPreemptively(Duration.ofMillis(10), () -> student.addCourse(lecturerCourseRecord));
+        }
+
+        @Nested
+        @DisplayName("Add Course to Student(Exceptional)")
+        @Tag("exceptional")
+        class AddCourseToStudentExceptionScenario {
+            @Test
+            @DisplayName("Got an exception when add a null lecturer course record to student")
+            void throwsExceptionWhenAddToNullCourseToStudent() {
+
+                final Student soner = new Student("1", "Soner", "Eren");
+
+                assertThatIllegalArgumentException().as("Throws IllegalArgumentException").isThrownBy(() -> soner.addCourse(null));
+
+                final Throwable throwable = catchThrowable(() -> soner.addCourse(null));
+                assertThat(throwable)
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("Cant be added null lecturer course record")
+                ;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/org/soner/course/unittest/courserecord/StudentTestWithAssertJAssertions.java
@@ -0,0 +1,93 @@
+package org.soner.course.unittest.courserecord;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StudentTestWithAssertJAssertions {
+
+
+    @Test
+    void createStudent() {
+        Student student = new Student("id1", "Soner", "Eren");
+        assertThat(student.getName()).as("Student's name %s", student.getName())
+                .isNotBlank()
+                .isEqualToIgnoringCase("Soner")
+                .isIn("Soner", "Omer", "Hakan")
+                .isNotIn("Ali", "Veli")
+                .startsWith("Son")
+                .doesNotStartWith("On")
+                .endsWith("er")
+                .contains("ner")
+                .contains(List.of("er", "Son", "oner"))
+                .hasSize(5);
+    }
+
+    @Test
+    void addCourseToStudent() {
+
+        final Student soner = new Student("id1", "Soner", "Eren", LocalDate.of(1994, 1, 1));
+        final Student orcun = new Student("id2", "Orcun", "Eren", LocalDate.of(1992, 1, 1));
+        final Student canan = new Student("id3", "Canan", "Sahin", LocalDate.of(1995, 1, 1));
+        final Student elif = new Student("id4", "Elif", "Oz", LocalDate.of(1991, 1, 1));
+        final Student hasan = new Student("id5", "Hasan", "Kartal", LocalDate.of(1990, 1, 1));
+
+        final List<Student> students = List.of(soner, orcun, canan, elif, hasan);
+
+        assertThat(students)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(5)
+                .contains(soner, orcun)
+                .contains(soner, Index.atIndex(0))
+                .containsOnly(soner, orcun, canan, hasan, elif) // listedeki herkesi sırası bağımsız söylemelisin!
+                .containsExactly(soner, orcun, canan, elif, hasan) // listedeki herkesi sıra önemli bir şekilde vermelisin!
+                .containsExactlyInAnyOrder(soner, orcun, canan, hasan, elif);
+
+        assertThat(students)
+                .filteredOn(student -> student.getBirthDate().until(LocalDate.now(), ChronoUnit.YEARS) >= 25)
+                .containsOnly(soner, orcun, canan, elif, hasan);
+
+        assertThat(students)
+                .filteredOn("birthDate", in(LocalDate.of(1994, 1, 1)))
+                .hasSize(1);
+
+        assertThat(students)
+                .extracting(Student::getName)
+                .filteredOn(name -> name.contains("e") || name.contains("E"))
+                .hasSize(2)
+                .containsOnly("Soner", "Elif");
+
+        assertThat(students)
+                .filteredOn(student -> student.getSurname().equals("Eren"))
+                .extracting(Student::getName, Student::getSurname)
+                .containsOnly(tuple("Soner", "Eren"), tuple("Orcun", "Eren"));
+
+
+        final LecturerCourseRecord lecturerCourseRecord101 = new LecturerCourseRecord(new Course("101"), new Semester());
+        final LecturerCourseRecord lecturerCourseRecord103 = new LecturerCourseRecord(new Course("103"), new Semester());
+        final LecturerCourseRecord lecturerCourseRecord105 = new LecturerCourseRecord(new Course("105"), new Semester());
+
+        soner.addCourse(lecturerCourseRecord101);
+        soner.addCourse(lecturerCourseRecord103);
+
+        canan.addCourse(lecturerCourseRecord101);
+        canan.addCourse(lecturerCourseRecord103);
+        canan.addCourse(lecturerCourseRecord105);
+
+        assertThat(students)
+                .filteredOn("name", in("Soner","Canan"))
+                .flatExtracting(student -> student.getStudentCourseRecords()) // collection extract etmek istersek!!!
+                .hasSize(5)
+                .filteredOn(studentCourseRecord -> studentCourseRecord.getLecturerCourseRecord().getCourse().getCode().equals("101"))
+                .hasSize(2);
+
+    }
+
+
+}
